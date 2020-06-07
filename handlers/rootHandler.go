c1482038c149ff263a6346556963b895c0b9adc0
Teppei Kanayama <kanayama.teppei@gmail.com>
2020-06-07 21:06:44 +0900

    second commit

--- handlers/rootHandler.go
+++ handlers/rootHandler.go
@@ -1,14 +1,1 @@
 package handlers
-
-import "net/http"
-
-// RootHandler is something
-func RootHandler(arg1 http.ResponseWriter, arg2 *http.Request) {
-	if arg2.URL.Path != "/" {
-		arg1.WriteHeader(http.StatusNotFound)
-		arg1.Write([]byte("Noot Found \n"))
-		return
-	}
-	arg1.WriteHeader(http.StatusOK)
-	arg1.Write([]byte("Okkkkkkkkkkkay!\n"))
-}
--- main.go
+++ main.go
@@ -1,14 +1,181 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"os"
-	"udemy-go-api/handlers"
+	"strings"
+
+	"github.com/asdine/storm"
+	"gopkg.in/mgo.v2/bson"
 )
 
+type jsonResponse map[string]interface{}
+
+// User holds data for a singlr user
+type User struct {
+	ID   bson.ObjectId `json:"id" storm:"id"`
+	Name string        `json:"name"`
+	Role string        `json:"role"`
+}
+
+const (
+	dbPath = "users.db"
+)
+
+// errors
+var (
+	ErrRecordInvalid = errors.New("invalid")
+)
+
+// All retrieves all users from the database
+func All() ([]User, error) {
+	db, err := storm.Open(dbPath)
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+	users := []User{}
+	if err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
+// One returns a single user record from the database
+func One(id bson.ObjectId) (*User, error) {
+	db, err := storm.Open(dbPath)
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+	u := new(User)
+	err = db.One("ID", id, u)
+	if err != nil {
+		return nil, err
+	}
+	return u, nil
+}
+
+// Delete removes a given record from the database
+func Delete(id bson.ObjectId) error {
+	db, err := storm.Open(dbPath)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+	u := new(User)
+	err = db.One("ID", id, u)
+	if err != nil {
+		return err
+	}
+	return db.DeleteStruct(u)
+}
+
+// Save updates or creates a given record in the database
+func (u *User) Save() error {
+	if err := u.validate(); err != nil {
+		return err
+	}
+	db, err := storm.Open(dbPath)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+	return db.Save(u)
+}
+
+// validate makes sure that the record contains valid data
+func (u *User) validate() error {
+	if u.Name == "" {
+		return ErrRecordInvalid
+	}
+	return nil
+}
+
+func usersGetAll(w http.ResponseWriter, r *http.Request) {
+	users, err := All()
+	if err != nil {
+		postError(w, http.StatusInternalServerError)
+		return
+	}
+	postBodyResponse(w, http.StatusOK, jsonResponse{"users": users})
+}
+
+func postError(w http.ResponseWriter, code int) {
+	http.Error(w, http.StatusText(code), code)
+}
+
+func postBodyResponse(w http.ResponseWriter, code int, content jsonResponse) {
+	if content != nil {
+		js, err := json.Marshal(content)
+		if err != nil {
+			postError(w, http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(code)
+		w.Write(js)
+		return
+	}
+}
+
+// UsersRooter is something
+func UsersRooter(w http.ResponseWriter, r *http.Request) {
+	path := strings.TrimSuffix(r.URL.Path, "/")
+
+	if path == "users/" {
+		switch r.Method {
+		case http.MethodGet:
+			usersGetAll(w, r)
+			return
+		case http.MethodPost:
+			return
+		default:
+			postError(w, http.StatusMethodNotAllowed)
+			return
+		}
+	}
+
+	path = strings.TrimSuffix(path, "/users/")
+	if !bson.IsObjectIdHex(path) {
+		postError(w, http.StatusNotFound)
+		return
+	}
+
+	// id := bson.ObjectIdHex(path)
+
+	switch r.Method {
+	case http.MethodGet:
+		return
+	case http.MethodPut:
+		return
+	case http.MethodPatch:
+		return
+	case http.MethodDelete:
+		return
+	default:
+		postError(w, http.StatusMethodNotAllowed)
+	}
+}
+
+// RootHandler is something
+func RootHandler(arg1 http.ResponseWriter, arg2 *http.Request) {
+	if arg2.URL.Path != "/" {
+		arg1.WriteHeader(http.StatusNotFound)
+		arg1.Write([]byte("Noot Found \n"))
+		return
+	}
+	arg1.WriteHeader(http.StatusOK)
+	arg1.Write([]byte("Okkkkkkkkkkkay!\n"))
+}
+
 func main() {
-	http.HandleFunc("/", handlers.RootHandler)
+	http.HandleFunc("/users/", UsersRooter)
+	http.HandleFunc("/users", UsersRooter)
+	http.HandleFunc("/", RootHandler)
 	err := http.ListenAndServe("localhost:11111", nil)
 	if err != nil {
 		fmt.Println(err)
