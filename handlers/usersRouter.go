1421690d5efc198e5d241410a4332ee22229e484
Teppei Kanayama <kanayama.teppei@gmail.com>
2020-06-07 22:44:01 +0900

    fix

--- handlers/usersRouter.go
+++ handlers/usersRouter.go
@@ -14,44 +14,7 @@ func UsersRooter(w http.ResponseWriter, r *http.Request) {
 	if path == "users/" {
 		switch r.Method {
 		case http.MethodGet:
-			usersGetAll(w, r)
-			return
-		case http.MethodPost:
-			return
-		default:
-			postError(w, http.StatusMethodNotAllowed)
-			return
-		}
-	}
-
-	path = strings.TrimSuffix(path, "/users/")
-	if !bson.IsObjectIdHex(path) {
-		postError(w, http.StatusNotFound)
-		return
-	}
-
-	// id := bson.ObjectIdHex(path)
-
-	switch r.Method {
-	case http.MethodGet:
-		return
-	case http.MethodPut:
-		return
-	case http.MethodPatch:
-		return
-	case http.MethodDelete:
-		return
-	default:
-		postError(w, http.StatusMethodNotAllowed)
-	}
-}
-func UsersRooter(w http.ResponseWriter, r *http.Request) {
-	path := strings.TrimSuffix(r.URL.Path, "/")
-
-	if path == "users/" {
-		switch r.Method {
-		case http.MethodGet:
-			usersGetAll(w, r)
+			// usersGetAll(w, r)
 			return
 		case http.MethodPost:
 			return
--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/Teppei-Kanayama/udemy-go-api/handlers"
-	"github.com/asdine/storm"
 	"gopkg.in/mgo.v2/bson"
 )
 
@@ -30,97 +28,32 @@ var (
 	ErrRecordInvalid = errors.New("invalid")
 )
 
-// All retrieves all users from the database
-func All() ([]User, error) {
-	db, err := storm.Open(dbPath)
-	if err != nil {
-		return nil, err
-	}
-	defer db.Close()
-	users := []User{}
-	if err != nil {
-		return nil, err
-	}
-	return users, nil
-}
-
-// One returns a single user record from the database
-func One(id bson.ObjectId) (*User, error) {
-	db, err := storm.Open(dbPath)
-	if err != nil {
-		return nil, err
-	}
-	defer db.Close()
-	u := new(User)
-	err = db.One("ID", id, u)
-	if err != nil {
-		return nil, err
-	}
-	return u, nil
-}
-
-// Delete removes a given record from the database
-func Delete(id bson.ObjectId) error {
-	db, err := storm.Open(dbPath)
-	if err != nil {
-		return err
-	}
-	defer db.Close()
-	u := new(User)
-	err = db.One("ID", id, u)
-	if err != nil {
-		return err
-	}
-	return db.DeleteStruct(u)
-}
-
-// Save updates or creates a given record in the database
-func (u *User) Save() error {
-	if err := u.validate(); err != nil {
-		return err
-	}
-	db, err := storm.Open(dbPath)
-	if err != nil {
-		return err
-	}
-	defer db.Close()
-	return db.Save(u)
-}
-
-// validate makes sure that the record contains valid data
-func (u *User) validate() error {
-	if u.Name == "" {
-		return ErrRecordInvalid
-	}
-	return nil
-}
-
-func usersGetAll(w http.ResponseWriter, r *http.Request) {
-	users, err := All()
-	if err != nil {
-		postError(w, http.StatusInternalServerError)
-		return
-	}
-	postBodyResponse(w, http.StatusOK, jsonResponse{"users": users})
-}
-
-func postBodyResponse(w http.ResponseWriter, code int, content jsonResponse) {
-	if content != nil {
-		js, err := json.Marshal(content)
-		if err != nil {
-			postError(w, http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(code)
-		w.Write(js)
-		return
-	}
-}
+// func usersGetAll(w http.ResponseWriter, r *http.Request) {
+// 	users, err := All()
+// 	if err != nil {
+// 		postError(w, http.StatusInternalServerError)
+// 		return
+// 	}
+// 	postBodyResponse(w, http.StatusOK, jsonResponse{"users": users})
+// }
+//
+// func postBodyResponse(w http.ResponseWriter, code int, content jsonResponse) {
+// 	if content != nil {
+// 		js, err := json.Marshal(content)
+// 		if err != nil {
+// 			postError(w, http.StatusInternalServerError)
+// 			return
+// 		}
+// 		w.Header().Set("Content-Type", "application/json")
+// 		w.WriteHeader(code)
+// 		w.Write(js)
+// 		return
+// 	}
+// }
 
 func main() {
-	http.HandleFunc("/users/", UsersRooter)
-	http.HandleFunc("/users", UsersRooter)
+	// http.HandleFunc("/users/", UsersRooter)
+	// http.HandleFunc("/users", UsersRooter)
 	http.HandleFunc("/", handlers.RootHandler)
 	err := http.ListenAndServe("localhost:11111", nil)
 	if err != nil {
