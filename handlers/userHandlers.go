dc35f2a668837af1e6bb58c2dc3bf1a9ea0b1f27
Teppei Kanayama <kanayama.teppei@gmail.com>
2020-06-07 22:52:18 +0900

    user handlers

---
+++ handlers/userHandlers.go
@@ -0,0 +1,13 @@
+package handlers
+
+//
+// import "net/http"
+//
+// func usersGetAll(w http.ResponseWriter, r *http.Request) {
+// 	// users, err := All()
+// 	if err != nil {
+// 		postError(w, http.StatusInternalServerError)
+// 		return
+// 	}
+// 	postBodyResponse(w, http.StatusOK, jsonResponse{"users": users})
+// }
--- main.go
+++ main.go
@@ -10,8 +10,6 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
-type jsonResponse map[string]interface{}
-
 // User holds data for a singlr user
 type User struct {
 	ID   bson.ObjectId `json:"id" storm:"id"`
@@ -28,32 +26,9 @@ var (
 	ErrRecordInvalid = errors.New("invalid")
 )
 
-// func usersGetAll(w http.ResponseWriter, r *http.Request) {
-// 	users, err := All()
-// 	if err != nil {
-// 		postError(w, http.StatusInternalServerError)
-// 		return
-// 	}
-// 	postBodyResponse(w, http.StatusOK, jsonResponse{"users": users})
-// }
-//
-// func postBodyResponse(w http.ResponseWriter, code int, content jsonResponse) {
-// 	if content != nil {
-// 		js, err := json.Marshal(content)
-// 		if err != nil {
-// 			postError(w, http.StatusInternalServerError)
-// 			return
-// 		}
-// 		w.Header().Set("Content-Type", "application/json")
-// 		w.WriteHeader(code)
-// 		w.Write(js)
-// 		return
-// 	}
-// }
-
 func main() {
-	// http.HandleFunc("/users/", UsersRooter)
-	// http.HandleFunc("/users", UsersRooter)
+	http.HandleFunc("/users/", handlers.UsersRooter)
+	http.HandleFunc("/users", handlers.UsersRooter)
 	http.HandleFunc("/", handlers.RootHandler)
 	err := http.ListenAndServe("localhost:11111", nil)
 	if err != nil {
